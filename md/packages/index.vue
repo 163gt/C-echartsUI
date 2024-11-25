513eadd85bc80cf9cde9f60d72463dd60bbb0f67
陳尘城 <91401952+163gt@users.noreply.github.com>
2024-11-25 14:30:39 +0800

    Add files via upload

---
+++ md/packages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="index">
+        787978979
+    </div>
+</template>
+<script>
+    export default{
+        name:'index'
+    }
+</script>
+<style scoped>
+.index{
+    width: 100px;
+    height: 100px;
+    background-color: aqua;
+}
+</style>
